de3fea8f206bed564537b60c1af56962a10bbe81
EdsonNSA <Edsonnunes360@gmail.com>
2025-09-26 11:51:55 -0300

    ajuste em api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,23 @@
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
 const api = axios.create({
-  baseURL: apiUrl
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
